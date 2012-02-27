3142b997883a960e2bbf0b8751c3cb6557879066
Luca Ferrari <fluca1978@gmail.com>
2012-02-27 17:41:21 +0000

    Fixed the replication workload to use the same configuration as other scripts.

--- bsdmag/02-replication/00-workload.sh
+++ bsdmag/02-replication/00-workload.sh
@@ -1,17 +1,33 @@
 #/bin/sh
 
+
 STANDBY_CLUSTER_NUMBER=$1
-MASTER_CLUSTER=/postgresql/cluster1
-DEST_CLUSTER=/postgresql/cluster${STANDBY_CLUSTER_NUMBER}
+POSTGRESQL_ROOT=/postgresql
+WAL_ARCHIVES=${POSTGRESQL_ROOT}/pitr
+MASTER_CLUSTER=${POSTGRESQL_ROOT}/cluster1
+DEST_CLUSTER=${POSTGRESQL_ROOT}/cluster${STANDBY_CLUSTER_NUMBER}
 RECOVERY_FILE=$DEST_CLUSTER/recovery.conf
-DEST_PORT=`psql -U bsdmag -A -t -c "SELECT setting FROM pg_settings WHERE name = 'port';" template1`
-DEST_PORT=`expr $DEST_PORT + $STANDBY_CLUSTER_NUMBER`
-echo "The stand-by node is listening on $DEST_PORT"
+
+if [ $# -le 0 ]
+then
+    echo "Please specify the number of the cluster to configure!"
+    exit
+fi
+
+if [ $STANDBY_CLUSTER_NUMBER -eq 1 ]
+then
+    echo "Cluster #1 is the master!"
+    exit
+fi
+
+
+echo "The stand-by node $STANDBY_CLUSTER_NUMBER is listening on $DEST_PORT"
 TEST_TABLE_NAME="test$$"
 COUNT_QUERY_MAGAZINE="SELECT count(*) FROM magazine;"
 COUNT_QUERY_TEST="SELECT count(*) FROM $TEST_TABLE_NAME;"
 
 echo "Inserting tuples into the master node"
+psql -U bsdmag -c "TRUNCATE TABLE magazine;" bsdmagdb
 psql -U bsdmag -c "INSERT INTO magazine(id, title) VALUES(generate_series(1,10000), 'TEST-REPLICA');" bsdmagdb
 echo "Tuples in the master node (magazine table)"
 psql -U bsdmag -A -t -c "$COUNT_QUERY_MAGAZINE" bsdmagdb
@@ -21,7 +37,7 @@ psql -U bsdmag -A -t -c "INSERT INTO $TEST_TABLE_NAME(pk, description) VALUES(ge
 
 echo "Activating the stand-by node..."
 sleep 30
-touch /postgresql/stand-by.${STANDBY_CLUSTER_NUMBER}.trigger
+touch /postgresql/standby.${STANDBY_CLUSTER_NUMBER}.trigger
 sleep 30
 
 echo "=========================================="
