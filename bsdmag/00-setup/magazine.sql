58f06c029673d2cfc0de15b5f66de9d7110eb02d
Luca Ferrari <fluca1978@gmail.com>
2012-04-12 08:07:41 +0000

    Fixed a problem with the initialization of the magazine table.

--- bsdmag/00-setup/magazine.sql
+++ bsdmag/00-setup/magazine.sql
@@ -14,11 +14,11 @@ INSERT INTO magazine (pk, id, month, issuedon, title)
 VALUES(1,'2012-01', 1,       '2012-01-01'::date ,     'FreeBSD: Get Up To Date');
 
 INSERT INTO magazine (pk, id, month, issuedon, title)
-VALUES(2,'2011-12', 12,       '2012-01-01'::date , 'Rolling Your Own Kernel');
+VALUES(2,'2011-12', 12,       '2012-04-01'::date , 'Rolling Your Own Kernel');
 
 
 INSERT INTO magazine (pk, id, month, issuedon, title)
-VALUES(3,'2011-11', 11,       '2012-01-01'::date, 'Speed Daemons');
+VALUES(3,'2011-11', 11,       '2011-01-01'::date, 'Speed Daemons');
 
 
 COMMIT;
