7d97884d408c599a649a52e7fc12c04f3458260f
Luca Ferrari <fluca1978@gmail.com>
2018-04-18 18:18:49 +0200

    Completed the example script for computing a codice fiscale.

--- examples/codice_fiscale.sql
+++ examples/codice_fiscale.sql
@@ -91,4 +91,156 @@ $BODY$
 LANGUAGE plpgsql;
 
 
+
+CREATE TABLE IF NOT EXISTS cf.places( code char(4) PRIMARY KEY,
+                                      description text NOT NULL,
+                                      UNIQUE( description ) );
+
+TRUNCATE cf.places;
+
+INSERT INTO cf.places( code, description )
+VALUES
+( 'F257', 'Modena' ),
+( 'D711', 'Formigine' ),
+( 'A944', 'Bologna' ),
+( 'F357', 'Serramazzoni' );
+
+CREATE OR REPLACE FUNCTION cf.cf_place( birth_place text )
+RETURNS char(4)
+AS $CODE$
+DECLARE
+  birth_code char(4);
+BEGIN
+  SELECT code
+  INTO STRICT birth_code
+  FROM cf.places
+  WHERE lower( description ) = lower( birth_place );
+
+  RETURN birth_code;
+END
+$CODE$
+LANGUAGE plpgsql;
+
+
+
+CREATE TABLE IF NOT EXISTS cf.check_chars( c char PRIMARY KEY,
+                                           odd_value int NOT NULL,
+                                           even_value int NOT NULL );
+TRUNCATE TABLE cf.check_chars;
+
+INSERT INTO cf.check_chars( c, odd_value, even_value )
+VALUES
+( '0', 1, 0 )
+,( '1', 0, 1 )
+,( '2', 5, 2 )
+,( '3', 7, 3 )
+,( '4', 9, 4 )
+,( '5', 13, 5 )
+,( '6', 15, 6 )
+,( '7', 17, 7 )
+,( '8', 19, 8 )
+,( '9', 21, 9 )
+,( 'A', 1, 0 )
+,( 'B', 0, 1 )
+,( 'C', 5, 2 )
+,( 'D', 7, 3 )
+,( 'E', 9, 4 )
+,( 'F', 13, 5 )
+,( 'G', 15, 6 )
+,( 'H', 17, 7 )
+,( 'I', 19, 8 )
+,( 'J', 21, 9 )
+,( 'K', 2, 10 )
+,( 'L', 4 , 11 )
+,( 'M', 18, 12 )
+,( 'N', 20, 13 )
+,( 'O', 11, 14 )
+,( 'P', 3, 15 )
+,( 'Q', 6, 16 )
+,( 'R', 8, 17 )
+,( 'S', 12, 18 )
+,( 'T', 14, 19 )
+,( 'U', 16, 20 )
+,( 'V', 10, 21 )
+,( 'W', 22, 22 )
+,( 'X', 25, 23 )
+,( 'Y', 24, 24 )
+,( 'Z', 23, 25 );
+
+
+
+CREATE OR REPLACE FUNCTION cf.cf_check( subject char(15) )
+RETURNS char
+AS $BODY$
+DECLARE
+  check_char char;
+  odd_sum int := 0;
+  even_sum int := 0;
+  i int;
+  current_value int;
+  final_value int;
+BEGIN
+
+  FOR i in 1..length( subject ) LOOP
+      IF i % 2 = 0 THEN
+         SELECT even_value
+         INTO STRICT current_value
+         FROM cf.check_chars
+         WHERE c = upper( substring( subject FROM i FOR 1 ) );
+
+         even_sum := even_sum + current_value;
+      ELSE
+        SELECT odd_value
+        INTO STRICT current_value
+        FROM cf.check_chars
+        WHERE c = upper( substring( subject FROM i FOR 1 ) );
+
+        odd_sum := odd_sum + current_value;
+     END IF;
+  END LOOP;
+
+
+  final_value := ( odd_sum + even_sum ) % 26;
+  RAISE DEBUG 'cf_check: % + % %% 26 = %', odd_sum, even_sum, final_value;
+
+  -- this is a trick: the remaining part
+  -- indicates the positional order of the letter
+  -- within the alphabet, which is
+  -- the values into the table excluding digits
+  SELECT c
+  INTO STRICT check_char
+  FROM cf.check_chars
+  WHERE even_value = final_value
+  AND c NOT IN ( '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' );
+
+  RETURN check_char;
+END
+$BODY$
+LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION cf.cf( surname text,
+                         name text,
+                         birth_date date,
+                         birth_place text,
+                         gender bool DEFAULT true )
+RETURNS char(16)
+AS $CODE$
+DECLARE
+  cf_string char(15);
+  cf_check  char(1);
+BEGIN
+  cf_string := cf.cf_letters( surname )
+               || cf.cf_letters( name )
+               || cf.cf_date( birth_date, gender )
+               || cf.cf_place( birth_place );
+
+   cf_check := cf.cf_check( cf_string );
+   RAISE DEBUG 'cf: % + %', cf_string, cf_check;
+
+  RETURN upper( cf_string || cf_check );
+END
+$CODE$
+LANGUAGE plpgsql;
+
 COMMIT;
