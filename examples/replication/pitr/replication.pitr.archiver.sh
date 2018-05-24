743ce14143844dc94598d30d6d11b4c42fc77390
Luca Ferrari <fluca1978@gmail.com>
2018-05-24 18:05:57 +0200

    Fixed strings and comment in archiver.

--- examples/replication/pitr/replication.pitr.archiver.sh
+++ examples/replication/pitr/replication.pitr.archiver.sh
@@ -10,14 +10,16 @@ then
     exit 2
 fi
 
-WAL_SEGMENT=$2  # %f
+WAL_SEGMENT=$2  # %f relative file name
 SRC_FILE=$3     # file with path (%p)
 DST_FILE=${ARCHIVING_PATH}/${WAL_SEGMENT}
 
-# non sovrascrivo il file se esiste gia!
+# do not override the WAL file on the
+# destination if it already exists
+# (this should never happen)
 if [ -f ${DST_FILE} ]
 then
-    echo "KO $NOW: $WAL_SEGMENT already existing" >> $LOG
+    echo "KO $NOW: $WAL_SEGMENT already exist!" >> $LOG
     exit 1
 fi
 
