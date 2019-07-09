e86d2f25034e7f645fcf31682f17333da73a1bcb
Luca Ferrari <fluca1978@gmail.com>
2019-07-09 12:18:31 +0200

    Added a procedure to issue alter table automatically.

--- examples/create_primary_keys.sql
+++ examples/create_primary_keys.sql
@@ -85,3 +85,67 @@ BEGIN
 END
 $CODE$
 LANGUAGE plpgsql;
+
+
+/*
+ * A procedure to automatically add primary keys using dynamic alter table statements.
+ * The procedure invokes the f_generate_primary_keys function and commits every change
+ * on a batch processing.
+ *
+ *
+ * \param pk_prefix the name or prefix of the column to be used as primary key (default 'pk')
+ * \param schemaz the schema in which look for wrong tables (default 'public')
+ * \param use_identity true if you want to use 'GENERATED ALWAYS AS IDENTITY' or false to use 'serial'
+ * \param append_table_name true if you want the column to be named pk_prefix + table-name (to avoid name clashes)
+ * \param commit_after the number of `ALTER TABLE` to issue before forcing a commit
+ *
+ *
+ * Example of invocation:
+testdb=# call p_generate_primary_keys( 'id', 'public', false, true );
+DEBUG:  Table [foo] without primary key
+DEBUG:   -> ALTER TABLE public.foo ADD COLUMN id_foo serial NOT NULL  PRIMARY KEY;
+DEBUG:  Table [bar] without primary key
+DEBUG:   -> ALTER TABLE public.bar ADD COLUMN id_bar serial NOT NULL  PRIMARY KEY;
+DEBUG:  Executing [ALTER TABLE public.foo ADD COLUMN id_foo serial NOT NULL  PRIMARY KEY;]
+DEBUG:  ALTER TABLE will create implicit sequence "foo_id_foo_seq" for serial column "foo.id_foo"
+DEBUG:  ALTER TABLE / ADD PRIMARY KEY will create implicit index "foo_pkey" for table "foo"
+DEBUG:  rewriting table "foo"
+DEBUG:  building index "foo_pkey" on table "foo" serially
+DEBUG:  Executing [ALTER TABLE public.bar ADD COLUMN id_bar serial NOT NULL  PRIMARY KEY;]
+DEBUG:  ALTER TABLE will create implicit sequence "bar_id_bar_seq" for serial column "bar.id_bar"
+DEBUG:  ALTER TABLE / ADD PRIMARY KEY will create implicit index "bar_pkey" for table "bar"
+DEBUG:  rewriting table "bar"
+DEBUG:  building index "bar_pkey" on table "bar" serially
+DEBUG:  Altered 2 tables in schema public
+LOG:  duration: 16.224 ms  statement: call p_generate_primary_keys( 'id', 'public', false, true );
+CALL
+
+ */
+CREATE OR REPLACE PROCEDURE p_generate_primary_keys( pk_prefix text DEFAULT 'pk',
+                                                     schemaz text DEFAULT 'public',
+                                                     use_identity boolean DEFAULT true,
+                                                     append_table_name boolean DEFAULT false,
+                                                     commit_after int DEFAULT 10 )
+AS $CODE$
+DECLARE
+  current_alter_table text;
+  done int := 0;
+BEGIN
+  FOR current_alter_table IN SELECT f_generate_primary_keys( pk_prefix, schemaz, use_identity, append_table_name )
+  LOOP
+    RAISE DEBUG 'Executing [%]', current_alter_table;
+    EXECUTE current_alter_table;
+    done := done + 1;
+
+
+    IF done % commit_after = 0 THEN
+       RAISE DEBUG 'Forcing a commit';
+       COMMIT;
+    END IF;
+
+  END LOOP;
+  RAISE DEBUG 'Altered % tables in schema %', done, schemaz;
+  COMMIT;
+END
+$CODE$
+LANGUAGE plpgsql;
