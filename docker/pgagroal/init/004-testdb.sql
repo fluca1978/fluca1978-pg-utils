e82557b9f5ffc94b84dfaa33d582b895c9edc08b
Luca Ferrari <fluca1978@gmail.com>
2025-02-27 17:01:35 +0100

    Initial work on pgagroal test demo
    Docker image for pgagroal inspired by
    <https://github.com/pgexporter/pgexporter/blob/main/contrib/docker/Dockerfile.rocky9>

---
+++ docker/pgagroal/init/004-testdb.sql
@@ -0,0 +1,94 @@
+-- create a new database
+CREATE DATABASE testdb WITH owner luca;
+
+-- now connect to the forum database
+-- and execute all the population statements
+\c testdb
+
+
+-- must be created by superuser, before changing
+-- the current role!
+CREATE EXTENSION IF NOT EXISTS plperl;
+CREATE EXTENSION IF NOT EXISTS bool_plperl;
+
+
+
+
+
+
+SET ROLE TO luca;
+
+CREATE SCHEMA luca AUTHORIZATION luca;
+
+
+CREATE TABLE scores
+  (
+    pk int GENERATED ALWAYS AS IDENTITY
+    , person text
+    , team   text
+    , class  text
+    , score  int DEFAULT 0
+    , PRIMARY KEY( pk )
+  );
+
+INSERT INTO scores( person, team, class, score )
+VALUES
+  ( 'Luca', 'Team-A', 'SM', 50 )  -- seniores maschile
+  , ( 'Alberto', 'Team-A', 'SM', 30 )  -- seniores maschile
+  , ( 'Filippo', 'Team-B', 'JM', 28 )
+  , ( 'Andrea', 'Team-B', 'JM', 22 )
+
+, ( 'Emanuela', 'Team-A', 'SF', 53 )
+, ( 'Barbara', 'Team-B', 'SF', 39 )
+, ( 'Simona', 'Team-A', 'JF', 29 )
+, ( 'Giada', 'Team-B', 'JF', 28 )
+;
+
+
+
+
+
+
+
+
+
+CREATE OR REPLACE FUNCTION
+check_codice_fiscale_perl( text )
+RETURNS bool
+TRANSFORM FOR TYPE bool
+AS $CODE$
+  $_[0] =~ /^ [A-Z]{6} \d{2} [A-Z] \d{2} [A-Z] \d{3} [A-Z] $/xi;
+$CODE$
+LANGUAGE plperl;
+
+
+
+
+
+
+
+
+
+CREATE TABLE IF NOT EXISTS luca.jbooks (
+       pk int generated always as identity
+       , content jsonb NOT NULL
+       , PRIMARY KEY( pk )
+);
+
+
+TRUNCATE TABLE luca.jbooks;
+GRANT ALL ON TABLE luca.jbooks TO luca;
+
+INSERT INTO luca.jbooks( content )
+VALUES ( '{ "title" : "Learn PostgreSQL",
+             "authors" : [ "Luca Ferrari", "Enrico Pirozzi" ],
+            "info" : {
+	         "year" : 2020, "edition" : 1 } }' )
+, ( '{ "title" : "PostgreSQL 11 Server Side Programming",
+       "authors" : [ "Luca Ferrari" ],        "info" : { "year" : 2018 }
+       }' )
+, ( '{ "title" : "Learn PostgreSQL",
+       "authors" : [ "Luca Ferrari", "Enrico Pirozzi" ],
+       "info" : {
+           "year" : 2020, "edition" : 2 } }' )
+;
