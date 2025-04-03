3df23d6e9f74893cb44b1bd545322a378c8a8bbb
Luca Ferrari <fluca1978@gmail.com>
2025-04-03 14:24:11 +0200

    pgagroal docker examples

--- docker/standalone/init/090-pgbench.sh
+++
@@ -1,5 +0,0 @@
-set -e
-psql -U postgres -c "create role pgbench with login password 'PgGench!';" postgres
-psql -U postgres -c "create database pgbench with owner pgbench;" postgres
-
-pgbench -i -U pgbench pgbench
