c5d39f4cf7efdf6ba3aa338aba1e86902d8a533c
Luca Ferrari <fluca1978@gmail.com>
2025-04-09 16:50:12 +0200

    Docker examples for logical replication

---
+++ Docker/logical_replication/init/001-create-database-users.sh
@@ -0,0 +1,5 @@
+set -e
+psql -U postgres -c "alter role postgres password 'P0stgres!!'" postgres
+psql -U postgres -c "create role luca with login password  'luca' connection limit 1" postgres
+
+psql -U postgres -c "create role rsync with login password  'rsync' replication " postgres
